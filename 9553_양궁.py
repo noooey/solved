032bf5d836a2627c4ed6289a8e3ea3321769e847
박규연 <20203065@kookmin.ac.kr>
2021-07-08 04:32:12 +0900

    9553_양궁
    그냥 각도에서 백터 사이의 각도로 코드 수정

--- 9553_양궁.py
+++ 9553_양궁.py
@@ -6,12 +6,9 @@ for i in range(T):
     N = int(input())
     for j in range(N):
         X1, Y1, X2, Y2 = map(int, input().split())
-        aa = X1**2 + Y1**2
-        bb = X2**2 + Y2**2
-        cc = (X1-X2)**2 + (Y1-Y2)**2
-        if aa == 0 or bb == 0:
-            exp += math.pi*2
+        if (X1 == -X2 and Y1 == -Y2) or (X1 == 0 and Y1 == 0) or (X2 == 0 and Y2 == 0):
+            break
         else:
-            exp += math.acos((aa + bb - cc)/(2*math.sqrt(aa)*math.sqrt(bb)))
+            exp += math.acos((X1*X2 + Y1*Y2)/math.sqrt((X1*X1 + Y1*Y1)*(X2*X2 + Y2*Y2)))
     print("{:.5f}".format(round(exp/(math.pi*2), 5)))
     exp = 0.00000
