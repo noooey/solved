33bfaefb4f37300fed1d38fd398089b67e58700a
박규연 <20203065@kookmin.ac.kr>
2021-07-13 05:14:35 +0900

    15372_A Simple Problem
    pypy, sys

--- 4172_sqrt log sin.py
+++ 4172_sqrt log sin.py
@@ -1,17 +1,14 @@
 import math
 import sys
 sys.setrecursionlimit(10**6)
-
 dict = {0: 1}
-
 def recursive(i):
+    # memoization 이용해서 시간초과 문제 해결
     if i in dict:
         return dict[i]
     else:
         dict[i] = recursive(math.floor(i-math.sqrt(i)))+recursive(math.floor(math.log(i)))+recursive(math.floor(i*math.sin(i)*math.sin(i)))
         return dict[i]
-
-
 while(True):
     i = int(sys.stdin.readline())
     if i == -1:
