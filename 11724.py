f6c1d7360ee8b0bec43ffca01b4909fbe68e96cb
kueyeon0429 <20203065@kookmin.ac.kr>
2022-09-28 21:03:44 +0900

    graph, dfs, 반례 체크...

--- 11724.py
+++ 11724.py
@@ -18,33 +18,24 @@ def BFS(root, graph):
 
 n, m = map(int, input().split())
 
-# 반례 주의~
-# 노드 n개, 간선 0개
-# 노드 0개, 간선 0개
-if m == 0:
-    if n == 0:
-        print(0)
+graph = {}
+for i in range(m):
+    n1, n2 = map(int, input().split())
+    if n1 not in graph:
+        graph[n1] = [n2]
     else:
-        print(n)
-else:
-    graph = {}
-    for i in range(m):
-        n1, n2 = map(int, input().split())
-        if n1 not in graph:
-            graph[n1] = [n2]
-        else:
-            graph[n1].append(n2)
-        if n2 not in graph:
-            graph[n2] = [n1]
-        else:
-            graph[n2].append(n1)
+        graph[n1].append(n2)
+    if n2 not in graph:
+        graph[n2] = [n1]
+    else:
+        graph[n2].append(n1)
 
-    cnt = 0
-    keys = list(graph.keys())
-    # 반례 주의~
-    # 혼자 있는 노드 개수 고려~
-    cnt += (n-len(keys))
-    while keys and graph:
-        keys = list(set(keys) - set(BFS(keys[0], graph)))
-        cnt += 1
-    print(cnt)
+cnt = 0
+keys = list(graph.keys())
+# 반례 주의~
+# 혼자 있는 노드 개수 고려~
+cnt += (n-len(keys))
+while keys and graph:
+    keys = list(set(keys) - set(BFS(keys[0], graph)))
+    cnt += 1
+print(cnt)
