ce8e1caf5626842bd4476a3ae201730b17d151f0
kueyeon0429 <20203065@kookmin.ac.kr>
2022-11-23 00:47:44 +0900

    dp

--- BOJ/1003.py
+++ BOJ/1003.py
@@ -1,24 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-cnt_0 = [1, 0, 1]
-cnt_1 = [0, 1, 1]
+zero = [1, 0, 1] # 0, 1, 2는 바로 리턴
+one = [0, 1, 1]
 
 def fibo(n):
-    if n == 0:
-        print(f'{cnt_0[0]} {cnt_1[0]}')
-        return
-    elif n == 1:
-        print(f'{cnt_0[1]} {cnt_1[1]}')
-        return
-    elif n == 2:
-        print(f'{cnt_0[2]} {cnt_1[2]}')
-        return
-    else:
-        cnt_0.append(cnt_0[n-1] + cnt_0[n-2])
-        cnt_1.append(cnt_1[n-1] + cnt_1[n-2])
-
-    print(f'{cnt_0[n-1]} {cnt_1[n-1]}')
+    if n >= len(zero):
+        for i in range(len(zero), n+1):
+            zero.append(zero[i-1] + zero[i-2])
+            one.append(one[i-1] + one[i-2])
+    print(f'{zero[n]} {one[n]}')
 
 t = int(input())
 for _ in range(t):
