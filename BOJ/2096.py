8db29b1e7809cea837b7944150d7aaecafcc9e1f
kueyeon0429 <20203065@kookmin.ac.kr>
2022-11-30 00:38:19 +0900

    dp, using minimal array

--- BOJ/2096.py
+++ BOJ/2096.py
@@ -1,36 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-def max_dp(num, lines, dp):
-    dp = [[0]*3 for _ in range(num)]
-    dp[0][0] = lines[0][0]
-    dp[0][1] = lines[0][1]
-    dp[0][2] = lines[0][2]
-
-    for i in range(1, num):
-        dp[i][0] = max(dp[i-1][0], dp[i-1][1]) + lines[i][0]
-        dp[i][1] = max(dp[i-1][0], dp[i-1][1], dp[i-1][2]) + lines[i][1]
-        dp[i][2] = max(dp[i-1][1], dp[i-1][2]) + lines[i][2]
-
-    return max(dp[n-1][0], dp[n-1][1], dp[n-1][2])
-
-def min_dp(num, lines, dp):
-    dp = [[0]*3 for _ in range(num)]
-    dp[0][0] = lines[0][0]
-    dp[0][1] = lines[0][1]
-    dp[0][2] = lines[0][2]
-
-    for i in range(num):
-        dp[i][0] = min(dp[i-1][0], dp[i-1][1]) + lines[i][0]
-        dp[i][1] = min(dp[i-1][0], dp[i-1][1], dp[i-1][2]) + lines[i][1]
-        dp[i][2] = min(dp[i-1][1], dp[i-1][2]) + lines[i][2]
-
-    return min(dp[n-1][0], dp[n-1][1], dp[n-1][2])
+dp_max = [0 for _ in range(3)]
+dp_min = [0 for _ in range(3)]
+tmp_max = [0 for _ in range(3)]
+tmp_min = [0 for _ in range(3)]
 
 n = int(input())
-n_list = []
-for _ in range(n):
-    n_list.append(list(map(int, input().split())))
 
-dp_table = [[0]*3 for _ in range(n)]
-print(max_dp(n, n_list, dp_table), min_dp(n, n_list, dp_table))
+for _ in range(n):
+    line = list(map(int, input().split()))
+
+    dp_max[0] = max(tmp_max[0], tmp_max[1]) + line[0]
+    dp_max[1] = max(tmp_max[0], tmp_max[1], tmp_max[2]) + line[1]
+    dp_max[2] = max(tmp_max[1], tmp_max[2]) + line[2]
+
+
+    dp_min[0] = min(tmp_min[0], tmp_min[1]) + line[0]
+    dp_min[1] = min(tmp_min[0], tmp_min[1], tmp_min[2]) + line[1]
+    dp_min[2] = min(tmp_min[1], tmp_min[2]) + line[2]
+
+    for j in range(3):
+        tmp_max[j] = dp_max[j]
+        tmp_min[j] = dp_min[j]
+
+print(max(dp_max), min(dp_min))
