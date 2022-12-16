2f2b03b6f65864ef6545c009c4eac34a8f49efa6
kueyeon0429 <20203065@kookmin.ac.kr>
2022-12-16 14:53:25 +0900

    just empliment

---
+++ BOJ/16968.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+def solutions(S):
+    cnt = 1
+    pre = S[0]
+    if pre == 'd':
+        cnt *= 10
+    else:
+        cnt *= 26
+
+    for chr in S[1:]:
+        print(f'pre!!! {pre} chr!!! {chr}')
+        if chr == 'd': # 숫자
+            tmp = cnt
+            cnt *= 10
+            if pre == chr:
+                cnt -= tmp
+            pre = chr
+        else: # 문자
+            tmp = cnt
+            cnt *= 26
+            if pre == chr:
+                cnt -= tmp
+            pre = chr
+    return cnt
+
+if __name__ == "__main__":
+    print(solutions(list(input().rstrip())))
