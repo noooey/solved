fc1a0e84d3c6f7bdf270524215103aba5dad32b9
kueyeon0429 <20203065@kookmin.ac.kr>
2023-01-26 18:41:22 +0900

    memoization, math, prime number

--- BOJ/4948.py
+++ BOJ/4948.py
@@ -2,17 +2,24 @@ from math import sqrt
 import sys
 input = sys.stdin.readline
 
-def is_prime_number(M):
+def is_sosu(M):
+    if M == 1:
+        return False
     for j in range(2, int(sqrt(M))+1):
         if M % j == 0:
             return False
     return True
 
+memo = []
+for k in range(2, 246912):
+    if is_sosu(k):
+        memo.append(k)
+
 def solutions(N):
     # N < 소수 <= 2N
     cnt = 0
-    for i in range(N+1, 2*N+1):
-        if is_prime_number(i):
+    for i in memo:
+        if N < i <= 2*N:
             cnt += 1
     return cnt
 
