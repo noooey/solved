8133d2ed2eba16e1213a3ec2d5d1140bf043672d
kueyeon0429 <20203065@kookmin.ac.kr>
2023-05-04 15:14:37 +0900

    :zap: update file name

---
+++ kmu-algolab/중간고사/비용.py
@@ -0,0 +1,16 @@
+t = int(input())
+for _ in range(t):
+    n, p = map(int, input().split())
+    n_list = sorted(list(map(int, input().split())))
+
+    i = 0
+    j = n-1
+    while True:
+        if n_list[i] + n_list[j] == p:
+            break
+        elif n_list[i] + n_list[j] < p:
+            i += 1
+        else:
+            j -= 1
+
+    print(n_list[i], n_list[j])
