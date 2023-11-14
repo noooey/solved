dca5d6beda280e3a32455432f2936cb3276623b5
noooey <20203065@kookmin.ac.kr>
2023-11-14 22:07:55 +0900

    LCS by memoization -> timeout

--- algorithm2023/hw4_dynamicProgramming/LCS_memo.py
+++ algorithm2023/hw4_dynamicProgramming/LCS_memo.py
@@ -1,22 +1,24 @@
-def LCS(X, Y):
-    m, n = len(X), len(Y)
-    memo = [[0] * (n + 1) for _ in range(m + 1)]
-
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if X[i - 1] == Y[j - 1]:
-                memo[i][j] = memo[i - 1][j - 1] + 1
+def LCS(memo, X, Y, m, n):
+    if m > 0 and n > 0:
+        if X[m-1] == Y[n-1]:
+            if memo[m-1][n-1] != 0:
+                memo[m][n] = memo[m-1][n-1] + 1
             else:
-                memo[i][j] = max(memo[i - 1][j], memo[i][j - 1])
-
-    return memo[m][n]
+                memo[m][n] = LCS(memo, X, Y, m-1, n-1) + 1
+            return memo[m][n]
+        else:
+            if memo[m-1][n] != 0 or memo[m][n-1] != 0:
+                memo[m][n] = max(memo[m-1][n], memo[m][n-1])
+                return memo[m][n]
+            return max(LCS(memo, X, Y, m-1, n), LCS(memo, X, Y, m, n-1))
+    else:
+        return 0
 
 t = int(input())
 for _ in range(t):
     x, y = input().split()
-    result = LCS(x, y)
-    print(result)
-
+    memo = [[0]*(len(y)+1) for _ in range(len(x)+1)]
+    print(LCS(memo, x, y, len(x), len(y)))
 
 
 """
