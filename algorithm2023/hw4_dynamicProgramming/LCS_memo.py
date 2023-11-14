48e20116026edfac160e90b62a466ba9d5ae3361
noooey <20203065@kookmin.ac.kr>
2023-11-14 22:40:03 +0900

    LCS by memoization

--- algorithm2023/hw4_dynamicProgramming/LCS_memo.py
+++ algorithm2023/hw4_dynamicProgramming/LCS_memo.py
@@ -1,24 +1,28 @@
 def LCS(memo, X, Y, m, n):
-    if m > 0 and n > 0:
-        if X[m-1] == Y[n-1]:
-            if memo[m-1][n-1] != 0:
-                memo[m][n] = memo[m-1][n-1] + 1
-            else:
-                memo[m][n] = LCS(memo, X, Y, m-1, n-1) + 1
-            return memo[m][n]
-        else:
-            if memo[m-1][n] != 0 or memo[m][n-1] != 0:
-                memo[m][n] = max(memo[m-1][n], memo[m][n-1])
-                return memo[m][n]
-            return max(LCS(memo, X, Y, m-1, n), LCS(memo, X, Y, m, n-1))
-    else:
+    if m == 0 or n == 0:
         return 0
 
-t = int(input())
-for _ in range(t):
-    x, y = input().split()
-    memo = [[0]*(len(y)+1) for _ in range(len(x)+1)]
-    print(LCS(memo, x, y, len(x), len(y)))
+    if memo[m][n] != -1:
+        return memo[m][n]
+
+    if X[m-1] == Y[n-1]:
+        memo[m][n] = LCS(memo, X, Y, m-1, n-1) + 1
+    else:
+        memo[m][n] = max(LCS(memo, X, Y, m-1, n), LCS(memo, X, Y, m, n-1))
+
+    return memo[m][n]
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        x, y = input().split()
+        memo = [[-1] * (len(y)+1) for _ in range(len(x)+1)]
+        result = LCS(memo, x, y, len(x), len(y))
+        print(result)
+
+if __name__ == "__main__":
+    main()
+
 
 
 """
